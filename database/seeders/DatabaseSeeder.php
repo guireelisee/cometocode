452c8c9011449729df2c2a7668231d3dd4184842
omar7840 <nanaomar062@gmail.com>
2021-06-19 22:00:23 +0000

    base Ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // UsersSeeder::class,
-            // ArrondissementsSeeder::class,
+            UsersSeeder::class,
+            ArrondissementsSeeder::class,
             SecteursSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
