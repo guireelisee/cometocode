f93300257b8cdc8a258694017fa02903ec6333fb
omar7840 <nanaomar062@gmail.com>
2021-06-19 20:05:46 +0000

    route change

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SocieteController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,19 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/societe', [App\Http\Controllers\SocieteController::class, 'index'])->name('societe')->middleware('societe');
-Route::post('/societe_submit', [App\Http\Controllers\SocieteController::class, 'store'])->name('societe.store')->middleware('societe');
-Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin')->middleware('admin');
+// Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin')->middleware('admin');
+
+Route::group(['namespace' => 'Admin',
+            'prefix' => 'admin',
+            'middleware' => 'admin'], function() {
+    Route::resource('societe', [SocieteController::class]);
+    Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+});
+Route::group(['namespace' => 'Societe',
+            'prefix' => 'societe',
+            'middleware' => 'societe'], function() {
+    Route::get('/societe', [App\Http\Controllers\SocieteController::class, 'index'])->name('societe')->middleware('societe');
+    Route::resource('societe', [SocieteController::class])->only(
+        'index','uptdate','edit'
+    );
+});
