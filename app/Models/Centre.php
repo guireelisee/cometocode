2ad4cc4a21471c447b83b80d2b642d13e259d9b7
guireelisee <guireelisee@gmail.com>
2021-06-20 05:01:20 +0000

    fix

--- app/Models/Centre.php
+++ app/Models/Centre.php
@@ -8,9 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Centre extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
     protected $guarded = ['id'];
-=======
-    protected $guarted = ['id'];
->>>>>>> admin
 }
